0ff48e5419ed6ca5c93681c9b423fe9df4f4ef06
Philip Woods <pwoods@hmc.edu>
2014-08-17 22:42:51 -0700

    Completed PE p18

--- project-euler/projecteuler.py
+++ project-euler/projecteuler.py
@@ -2,7 +2,7 @@
 # Contains the functions that I used to solve each problem in Project Euler.
 
 # Problems solved:   1   2   3   4   5   6   7   8   9  10
-#                   11  12  13  14  15  16  17      19  20
+#                   11  12  13  14  15  16  17  18  19  20
 #                                   25
 #                               34
 
