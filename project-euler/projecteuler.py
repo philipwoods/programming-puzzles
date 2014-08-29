bc02d2ae3a82449708648431dbd78705a078c6bb
Philip Woods <pwoods@hmc.edu>
2014-08-29 05:06:44 -0700

    Complete PE P21 and start P22

--- project-euler/projecteuler.py
+++ project-euler/projecteuler.py
@@ -3,7 +3,7 @@
 
 # Problems solved:   1   2   3   4   5   6   7   8   9  10
 #                   11  12  13  14  15  16  17  18  19  20
-#                                   25
+#                   21              25
 #                               34
 #                   
 #                   
@@ -679,6 +679,8 @@ def amicable(maximum):
             total += d
             total += n
     return total
+
+# PROBLEM 22
 
 
 
