cb5b99a739bd8881c6777c027921f3ae85359baa
Philip Woods <pwoods@hmc.edu>
2014-08-29 19:34:55 -0700

    Solved PE P48

--- project-euler/projecteuler.py
+++ project-euler/projecteuler.py
@@ -3,9 +3,9 @@
 
 # Problems solved:   1   2   3   4   5   6   7   8   9  10
 #                   11  12  13  14  15  16  17  18  19  20
-#                   21              25
+#                   21  22          25
 #                               34
-#                   
+#                                               48
 #                   
 #                                           67
 
@@ -703,7 +703,19 @@ def nameScores():
         totalScore += nameScore*(names.index(name)+1)
     return totalScore
 
+# PROBLEM 48
 
+def lastDigits(maximum, numDigits):
+    """
+    Returns the last numDigits digits of the self-power sum up to maximum.
+    E.g. 1^1 + 2^2 + 3^3 + ... + max^max
+    """
+    digitSum = 0
+    for n in range(maximum):
+        selfPower = (n+1)**(n+1)
+        lastDigits = selfPower % 10**numDigits
+        digitSum += lastDigits
+    return digitSum % 10**numDigits
 
 
 
